3df524d9a23414ad191fcd7c8bf7d53abb14b77c
Ktal-g <68426898+Ktal-g@users.noreply.github.com>
2021-04-25 10:58:14 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -16,10 +16,8 @@ import plotly.io as pio
 pio.renderers.default = 'browser'
 import networkx as nx
 import numpy as np
-import sys
+
 import tensorflow as tf
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'D:/Jack/glacier_proj_home/Codeing_files/functions')
 
 tf.compat.v1.enable_eager_execution()
 
@@ -38,14 +36,14 @@ glacier_list = [
 'RGI60-14.03334',
 'RGI60-15.06881',
 'RGI60-15.06977',
-'RGI60-15.06720', #13 originat tuning sample size
+'RGI60-15.06720', 
 'RGI60-13.53689',
 'RGI60-13.52478',
 'RGI60-13.53689',
 'RGI60-13.54059',
 'RGI60-14.02849',
 'RGI60-14.02653',
-'RGI60-14.03405', #20
+'RGI60-14.03405', #20 originat tuning sample size
 'RGI60-14.01226',
 'RGI60-14.04477',
 'RGI60-14.04638',
@@ -102,7 +100,7 @@ file_id = [
 '2370to2400',
 '2580to2610',
 '2580to2610',
-'2580to2610', #originat tuning sample size
+'2580to2610', # original tuning sample size
 '2340to2370',
 '2340to2370',
 '2340to2370',
@@ -173,13 +171,12 @@ evaluation_glacier_file_ids = [
 networks = glacier_network()
 
 #%%
-#%%
 #Training glaciers
 for file_i, glacier_id in enumerate(glacier_list):
         
     print("file num and glacier: ", file_i, glacier_id)
         
-    if file_i < 200: 
+    if file_i < 20:  #to controle howmany training glaciers to use
             networks.Add_GlacierFromFileId(glacier_id, file_id = file_id[file_i], training = True)
         
 #%%Analysis glaciers
@@ -188,7 +185,7 @@ for file_i, glacier_id in enumerate(evaluation_glaciers):
         
     networks.Add_GlacierFromFileId(glacier_id, file_id = evaluation_glacier_file_ids[file_i], training = False)
 #%%
-#features
+#choose features
 node_features = ['width', 'mean velocity', 'dS', 'dY']
 edge_features = []#['area', '_precip', 'area_temp', 'P_diff']
 inflow_features=['mean thickness']
@@ -196,55 +193,25 @@ inflow_features=['mean thickness']
 #ANN model hyper params
 n_layers = 6
 n_nodes = 25
-exclude_filter_lowwer = 0
+
+#want to prevent the model from training on a mean thickness range
+exclude_filter_lowwer = 0 
 exclude_filter_upper = 0
 
-#Training hyperparams
-optimizer_num = [3,3]   #optimizer_names = 'Adagrad', 'Adadelta', 'Adam', 'Nadam', 'RMSprop', 'SGD', 'NSGD'
+#Training hyperparams, these are the values for the informed and blind NN respectively that were tuned for 20 glaciers.
+optimizer_num = [2,3]   #optimizer_names = 'Adagrad', 'Adadelta', 'Adam', 'Nadam', 'RMSprop', 'SGD', 'NSGD'
 epochs = 80      #number of times to train on training data
 drop_frac = [0.02, 0.05]
-validation_split =[0.3,0.3]
+validation_split =[0.5,0.5]
 
-#%%
-#"informed-model, training samples: 20 (1)", "informed-model, training samples: 20 (2)", "informed-model, training samples: 20 (3)"
-#"informed-model, training samples: 25 (1)", "informed-model, training samples: 25 (2)", "informed-model, training samples: 25 (3)"
-#"informed-model, training samples: 30 (1)", "informed-model, training samples: 30 (2)", "informed-model, training samples: 30 (3)"
-#"informed-model, d=0 training samples: 30 (1)", "informed-model, d=0 training samples: 30 (2)", "informed-model, d=0 training samples: 30 (3)"
-#"blind-model, training samples: 20 (1)", "blind-model, training samples: 20 (2)", "blind-model, training samples: 20 (3)"
-#"blind-model, training samples: 25 (1)", "blind-model, training samples: 25 (2)", "blind-model, training samples: 25 (3)"
-#"blind-model, training samples: 30 (1)", "blind-model, training samples: 30 (2)", "blind-model, training samples: 30 (3)"
+#%% rough method for creating multimple models at once, add a name to the list and a hyperparameters list to the parameters list.
 model_names = ["informed-model, d=.05 training samples: 20, filter 100 to 150"]
 parameters = [[0.02, 0.5, 2, 'relu', True]]
-
-
-#model_names = ['informed: Adam small', 'blind: Nadam small']#['informed: Adagrad', 'informed: Adadelta', 'informed: Adam', 'informed: Nadam', 'informed: RMSprop', 'informed: SGD', 'informed: NSGD']#, 'informed-model: sigmoid, d=.01']
-#parameters = [[0.02,0.5, 2, True],[0.05, 0.5, 3, False]]
 
 #%%train model
 for i, model in enumerate(model_names):
     networks.Train_Model(model_name = model_names[i],
-                         training_glaciers =[
-'RGI60-13.37523',
-'RGI60-13.30888',
-'RGI60-13.26415',
-'RGI60-13.54431',
-'RGI60-13.43528',
-'RGI60-14.04668',
-'RGI60-14.02150',
-'RGI60-14.01670',
-'RGI60-14.00449',
-'RGI60-14.03334',
-'RGI60-15.06881',
-'RGI60-15.06977',
-'RGI60-15.06720', #13 originat tuning sample size
-'RGI60-13.53689',
-'RGI60-13.52478',
-'RGI60-13.53689',
-'RGI60-13.54059',
-'RGI60-14.02849',
-'RGI60-14.02653',
-'RGI60-14.03405', #20
-],
+                         training_glaciers =[], #if empty will use all the glaciers in the networks object that are for training
                          node_feature_vairables=node_features,
                          inflow_features=inflow_features,
                          edge_features=edge_features, 
@@ -254,8 +221,10 @@ for i, model in enumerate(model_names):
                          include_edge_features=parameters[i][4])
 
 
-#%%Pop glaciers that don't work with training:
-#remove glacier: 'RGI60-14.05872', 'RGI60-15.00621'
+#%%Pop glaciers that don't work with training: 
+#!!! this is because when getting feature data the glacier network cant process two tributaries meeting at the same point.
+#this is an easy fix, I will get round to it when I have more time, but don't think it affects the results of the trained models.
+#remove glaciers: 'RGI60-14.05872', 'RGI60-15.00621'
 remove_glacier = 'RGI60-14.05872'
 if remove_glacier in networks.nxGraphDict.keys(): 
     networks.nxGraphDict.pop(remove_glacier)
@@ -265,9 +234,7 @@ if remove_glacier in networks.nxGraphDict.keys():
     networks.nxGraphDict.pop(remove_glacier)
     print("removed glacier: ", remove_glacier)
 #%%
-networks.plot_epoch_RMSE(vairable='train size',models=["informed-model, training samples: 20 (1)", "informed-model, training samples: 20 (2)", "informed-model, training samples: 20 (3)"
-                                                       ,"informed-model, training samples: 25 (1)", "informed-model, training samples: 25 (2)", "informed-model, training samples: 25 (3)"
-                                                       ,"informed-model, training samples: 30 (1)", "informed-model, training samples: 30 (2)","informed-model, training samples: 30 (3)"], figsize=(12,10))
+networks.plot_epoch_RMSE(vairable='train size',models=model_names, figsize=(12,10))
 #%%
 
 glacier_to_test_flowline_thickness = {'Biafo glacier': {'RGI_id':"RGI60-14.00005", "main flowline":'17'},
@@ -275,9 +242,10 @@ glacier_to_test_flowline_thickness = {'Biafo glacier': {'RGI_id':"RGI60-14.00005
                                       'Rongbuk Glacier':{'RGI_id':'RGI60-15.09991', "main flowline":'5'},
                                       'Ngozumpa':{'RGI_id':'RGI60-15.03473', "main flowline":'6'}}
 
-networks.Plot_Network(glacier_to_test_flowline_thickness['Ngozumpa']['RGI_id'], 'mean thickness', plot=True)
+networks.Plot_Network(glacier_to_test_flowline_thickness['Ngozumpa']['RGI_id'], 'mean thickness', plot=True) 
+#if adding a glacier to the test glaciers, plot network and pick the main flowline
 
-#%%
+#%% all test glaciers
 #from the network choose the flowline/catchment number
 for glacier in glacier_to_test_flowline_thickness.keys():
     networks.Plot_GlacierThickness(RGI_id = glacier_to_test_flowline_thickness[glacier]['RGI_id'], 
@@ -293,7 +261,7 @@ print(glacier_to_test_flowline_thickness[glacier]["main flowline"])
 networks.Plot_GlacierThickness(RGI_id = glacier_to_test_flowline_thickness[glacier]['RGI_id'], 
                                    catchment_number = glacier_to_test_flowline_thickness[glacier]["main flowline"],
                                    glacier_name = glacier,
-                                   model_names=["informed-model, d=.02 training samples: 20, filter 100 to 150", "blind-model, d=.05 training samples: 20, filter 100 to 150"],
+                                   model_names=model_names,
                                    model_custom_names=[])
 #%%
 
@@ -310,28 +278,9 @@ target_feature = 'mean thickness'
 #%%
 
 networks.Test_ModelFeature(feature = 'mean thickness',
-                           models=["informed-model, d=.02 training samples: 20, filter 100 to 150", "blind-model, d=.05 training samples: 20, filter 100 to 150"],
-                           model_custom_names=["informed-model, training samples: 20", "blind-model, training samples: 20"],
-                           test_glaciers = test_glaciers, training_glaciers=['RGI60-13.37523',
-'RGI60-13.30888',
-'RGI60-13.26415',
-'RGI60-13.54431',
-'RGI60-13.43528',
-'RGI60-14.04668',
-'RGI60-14.02150',
-'RGI60-14.01670',
-'RGI60-14.00449',
-'RGI60-14.03334',
-'RGI60-15.06881',
-'RGI60-15.06977',
-'RGI60-15.06720', #13 originat tuning sample size
-'RGI60-13.53689',
-'RGI60-13.52478',
-'RGI60-13.53689',
-'RGI60-13.54059',
-'RGI60-14.02849',
-'RGI60-14.02653',
-'RGI60-14.03405'], 
+                           models=model_names,
+                           model_custom_names=[],
+                           test_glaciers = test_glaciers, training_glaciers=[], 
                            feature_bin_size=20, bar_width = 8, tick_num = 10,
                            figsize=(14,8))
 
